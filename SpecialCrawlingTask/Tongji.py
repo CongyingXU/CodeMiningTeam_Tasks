bd47641a8d5c273e665136c54a84844d34fc53b1
congyingXU <1084729816@qq.com>
2020-02-21 23:42:12 +0800

    new task todo list

--- SpecialCrawlingTask/Tongji.py
+++ SpecialCrawlingTask/Tongji.py
@@ -15,34 +15,46 @@ from CommonFunction import JSONFIle_processing, File_processing
 import json
 # file+path
 
-
+# jar_path = "/Users/congyingxu/Downloads/CrawledGAVs_0218/"
 jar_path = "/home/hadoop/dfs/data/Workspace/CongyingXU/Passport/Processed_Downloads_Jars/CrawledGAVs_0218/"
-# file_path = "/home/hadoop/dfs/data/Workspace/projects_DG/harmo_gav.json"
-# file_path = "/Users/congyingxu/Desktop/harmo_gav.json"
-file_path = '/home/hadoop/dfs/data/Workspace/CongyingXU/gav_all_mt_1000_ic.json'
 
-task_file_path = 'Local_Data/gav_all_mt_1000_ic.json'
-task_list = []
-ele_list = []
+kaifeng_tofo_path = "Local_Data/kaifeng_FSE_harmo_gav.json"
+wangying_todo_gradle_path = "Local_Data/wangying_FSE_Todo_GAV_List_gradle.json"
+wangying_todo_maven_path = "Local_Data/wangying_FSE_Todo_GAV_List_maven.json"
 
+
+wangying_todo_gradle = []
+wangying_todo_maven = []
 
 Task_todo_list =  []
-
-count_less = 0
-
+Existing_todo_list = []
 
 def read():
-    global ele_list,task_list
-    with open(file_path,'r') as f:
-        ele_list = json.loads( f.read() )
-    # ele_list = JSONFIle_processing.read(file_path)
-    # task_list = JSONFIle_processing.read(task_file_path)
+    global wangying_todo_gradle,wangying_todo_maven, kaifeng_tofo
+    wangying_todo_gradle = JSONFIle_processing.read( wangying_todo_gradle_path )
+    wangying_todo_maven = JSONFIle_processing.read( wangying_todo_maven_path )
+    kaifeng_tofo = JSONFIle_processing.read(kaifeng_tofo_path)
+
 
 def compare():
-    global count_less,Task_todo_list
+    global  wangying_todo_gradle,wangying_todo_maven, kaifeng_tofo, Task_todo_list
 
-    Jars_num = len( File_processing.walk_FileDir(jar_path) )
-    print("Jars_num: ", Jars_num)
+
+    # huizong todo
+    for ele in wangying_todo_gradle:
+        if ele not in Task_todo_list:
+            Task_todo_list.append( ele )
+    for ele in wangying_todo_maven:
+        if ele not in Task_todo_list:
+            Task_todo_list.append( ele )
+    for ele in kaifeng_tofo:
+        if ele not in Task_todo_list:
+            Task_todo_list.append( ele )
+
+
+    # get need todo
+    existing_Jars_full_list = len( File_processing.walk_FileDir(jar_path) )
+    print("Jars_num: ", existing_Jars_full_list)
 
     GA_folder_list = File_processing.walk_L1_Folders(jar_path)
     GA_folder_map = {}
@@ -50,7 +62,10 @@ def compare():
         GA = folder.split("__fdse__")[0] +'__fdse__'+ folder.split("__fdse__")[1]
         GA_folder_map[GA]= folder
 
-    for ele in ele_list:
+
+    count_less = 0
+
+    for ele in Task_todo_list:
         try:
             groupId = ele["groupId"]
             artifactId = ele["artifactId"]
@@ -58,47 +73,22 @@ def compare():
 
             full_path = jar_path + GA_folder_map[groupId + '__fdse__' + artifactId] + '/' + version +'/'
 
-
             if os.path.exists(full_path):
                 continue
             else:
                 count_less += 1
-                # print(file_path)
-                Task_todo_list.append(ele  )
+                Existing_todo_list.append(ele)
         except:
             continue
 
     print(count_less)
-    # JSONFIle_processing.write(Task_todo_list,'Todo_GAV_List.json')
 
+    JSONFIle_processing.write(Task_todo_list,'Local_Data/Original_All_Todo_GAV_List.json')
+    JSONFIle_processing.write(Existing_todo_list,"Local_Data/Existing_Todo_GAV_List.json")
 
-
-
-def compare_tasks():
-    global count_less
-
-    for ele in ele_list:
-        try:
-            groupId = ele["groupId"]
-            artifactId = ele["artifactId"]
-            version = ele["version"]
-
-            if ele in task_list:
-                pass
-            else:
-                count_less += 1
-        except:
-            continue
-
-
-    print(count_less)
-
-def main_taskFile():
-    read()
-    compare_tasks()
 
 def main_Serve():
-    read()
+    # read()
     compare()
 
 if __name__ == '__main__':
