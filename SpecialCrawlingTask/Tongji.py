86748968241334b8639f468ff187221a2818f220
congyingXU <1084729816@qq.com>
2020-02-21 11:00:54 +0800

    生成new task

--- SpecialCrawlingTask/Tongji.py
+++ SpecialCrawlingTask/Tongji.py
@@ -18,19 +18,27 @@ import json
 
 jar_path = "/home/hadoop/dfs/data/Workspace/CongyingXU/Passport/Processed_Downloads_Jars/CrawledGAVs_0218/"
 file_path = "/home/hadoop/dfs/data/Workspace/projects_DG/harmo_gav.json"
+# file_path = "/Users/congyingxu/Desktop/harmo_gav.json"
+
+task_file_path = 'Local_Data/gav_all_mt_1000_ic.json'
+task_list = []
 ele_list = []
+
+
+Task_todo_list =  []
 
 count_less = 0
 
 
 def read():
-    global ele_list
+    global ele_list,task_list
     with open(file_path,'r') as f:
         ele_list = json.loads( f.read() )
     # ele_list = JSONFIle_processing.read(file_path)
+    task_list = JSONFIle_processing.read(task_file_path)
 
 def compare():
-    global count_less
+    global count_less,Task_todo_list
 
     GA_folder_list = File_processing.walk_L1_Folders(jar_path)
     GA_folder_map = {}
@@ -51,18 +59,43 @@ def compare():
                 continue
             else:
                 count_less += 1
+                # print(file_path)
+                Task_todo_list.append(ele  )
         except:
             continue
 
     print(count_less)
+    JSONFIle_processing.write(Task_todo_list,'Todo_GAV_List.json')
+
+
+
+
+def compare_tasks():
+    global count_less
+
+    for ele in ele_list:
+        try:
+            groupId = ele["groupId"]
+            artifactId = ele["artifactId"]
+            version = ele["version"]
+
+            if ele in task_list:
+                pass
+            else:
+                count_less += 1
+        except:
+            continue
+
+
+    print(count_less)
+
+def main_taskFile():
+    read()
+    compare_tasks()
 
 def main_Serve():
     read()
     compare()
 
-def main_taskFile():
-    pass
-
 if __name__ == '__main__':
-    read()
-    compare()
+    main_Serve()
