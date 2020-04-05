f862f1fee32c3c5900a195f7ccf1923a5ff5a54a
congyingXU <1084729816@qq.com>
2020-04-05 21:29:02 +0800

    test 爬取NVD的cpe range信息

--- CVE_HW/CrawlerNVD/NVDCpeRangesCrawler.py
+++ CVE_HW/CrawlerNVD/NVDCpeRangesCrawler.py
@@ -55,8 +55,8 @@ def session_get_url(url):
     print('------------------------- url:' + url)
     session = requests.Session()
     session.headers = {
-        # 'User-Agent': random.choice(UserAgents.agents),
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
+        'User-Agent': random.choice(UserAgents.agents),
+        # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
         'Host': 'nvd.nist.gov',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
