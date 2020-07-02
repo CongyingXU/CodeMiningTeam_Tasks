e765a50c45d458bab6fa481ed14c94dce7167441
congyingXU <1084729816@qq.com>
2020-07-02 11:52:34 +0800

    NER-Study: try 13cate 1:0:1 train and test

--- NER_Study/GenerateAllDatasetText.py
+++ NER_Study/GenerateAllDatasetText.py
@@ -54,9 +54,9 @@ def main():
             # print(test_dataset_list)
 
             # extent dataset list
-            all_train_list.extend( cate_dataset[:int(len_cate_dataset *1/3)] )
-            all_valid_list.extend( cate_dataset[int(len_cate_dataset *1/3) : int(len_cate_dataset *2/3)] )
-            all_test_list.extend( cate_dataset[int(len_cate_dataset *2/3) : ] )
+            all_train_list.extend( cate_dataset[:int(len_cate_dataset *1/2)] )
+            # all_valid_list.extend( cate_dataset[int(len_cate_dataset *1/3) : int(len_cate_dataset *2/3)] )
+            all_test_list.extend( cate_dataset[int(len_cate_dataset *1/2) : ] )
             print(len(all_train_list), len(all_valid_list), len(all_test_list))
 
             #write categ
@@ -64,11 +64,11 @@ def main():
             cate_valid = ''
             cate_test = ''
 
-            for ele in cate_dataset[:int(len_cate_dataset *1/3)]:
+            for ele in cate_dataset[:int(len_cate_dataset *1/2)]:
                 cate_train += ele + '\n\n'
-            for ele in cate_dataset[int(len_cate_dataset *1/3) : int(len_cate_dataset *2/3)]:
-                cate_valid += ele + '\n\n'
-            for ele in cate_dataset[int(len_cate_dataset *2/3) : ]:
+            # for ele in cate_dataset[int(len_cate_dataset *1/3) : int(len_cate_dataset *2/3)]:
+            #     cate_valid += ele + '\n\n'
+            for ele in cate_dataset[int(len_cate_dataset *1/2) : ]:
                 cate_test += ele + '\n\n'
 
             File_processing.write_TXTfile(folder + 'integrated_dataset/' +categ_name + '_train.txt', cate_train)
