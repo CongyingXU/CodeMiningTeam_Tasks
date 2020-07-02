696d305542f7230afa30f06f0a15e09f5cf3de40
congyingXU <1084729816@qq.com>
2020-07-02 22:27:44 +0800

    NER-Study: try 13cate 3:1:1 train and test 40epochs (86server)

--- NER_Study/RunKashgariEnghish.py
+++ NER_Study/RunKashgariEnghish.py
@@ -10,7 +10,7 @@ sklearn/metrics/classification.py
 def accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):
 """
 
-import sys
+import os, sys, inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
